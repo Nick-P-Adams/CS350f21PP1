3e80ccc3cd616e01a1655fd0511e5e9a337c7ac4
Thedon989 <45801555+Thedon989@users.noreply.github.com>
2021-11-14 17:11:21 -0800

    Coded up LoaderFix class
    Loader fix pretty much finished and compiles. However, still need to test it also I am not exactly sure what we are doing with the hashmap but for now I am just adding the navaids to them.

--- src/atcsim/loader/navaid/LoaderFix.java
+++ src/atcsim/loader/navaid/LoaderFix.java
@@ -4,9 +4,11 @@ import java.io.IOException;
 import java.util.Map;
 import java.util.Scanner;
 
+import atcsim.datatype.*;
 import atcsim.graphics.view.navigation.OverlayNavigation;
 import atcsim.loader.A_Loader;
 import atcsim.world.navigation.A_ComponentNavaid;
+import atcsim.world.navigation.ComponentNavaidFix;
 
 public class LoaderFix extends A_Loader 
 {
@@ -22,7 +24,18 @@ public class LoaderFix extends A_Loader
 		
 		while(!(nextLine.trim().equals("")))
 		{
-			System.out.println(nextLine);
+			String[] specParts = nextLine.split(",");
+			
+			Latitude latitude = new Latitude(Integer.parseInt(specParts[1].trim()), Integer.parseInt(specParts[2].trim()), Double.parseDouble(specParts[3].trim()));
+			Longitude longitude = new Longitude(Integer.parseInt(specParts[4].trim()), Integer.parseInt(specParts[5].trim()), Double.parseDouble(specParts[6].trim()));
+			Altitude altitude = new Altitude(Double.parseDouble(specParts[7].trim()));
+			
+			CoordinateWorld3D position = new CoordinateWorld3D(latitude, longitude, altitude);
+			ComponentNavaidFix fix = new ComponentNavaidFix(specParts[0].trim(), position);
+			
+			this.navaids.put(specParts[0].trim(), fix);
+			this.overlay.addNavaid(fix);
+			
 			nextLine = scanner.nextLine();
 		}
 	}
