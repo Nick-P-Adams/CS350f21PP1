292d17a057f5f6be705b8efd4b89a71a35134f62
Thedon989 <45801555+Thedon989@users.noreply.github.com>
2021-12-02 00:40:47 -0800

    Completed Altitude test
    It's kinda late so I just decided to start at the top of the list with the altitude test. Gonna work on coordinateWorld3D next

--- src/atcsim/part2/DatatypeTester.java
+++ src/atcsim/part2/DatatypeTester.java
@@ -1,5 +1,7 @@
 package atcsim.part2;
 
+import atcsim.datatype.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
@@ -7,8 +9,31 @@ import org.junit.jupiter.api.Test;
 class DatatypeTester {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testAltitude() 
+	{
+		Altitude a1 = new Altitude(1000);
+		Altitude a2 = new Altitude(200);
+		
+		double altitudeTest = a1.getValue_() + a2.getValue_();
+		assertEquals(1200, altitudeTest);
+		
+		altitudeTest = a2.getValue_() + a1.getValue_();
+		assertEquals(1200, altitudeTest);
+		
+		altitudeTest = a1.getValue_() - a2.getValue_();
+		assertEquals(800, altitudeTest);
+		
+		altitudeTest = a2.getValue_() - a1.getValue_();
+		assertEquals(-800, altitudeTest);
+		
+		int altCompareTest = a1.compareTo(a1);
+		assertEquals(0, altCompareTest);
+		
+		altCompareTest = a2.compareTo(a1);
+		assertEquals(-1, altCompareTest);
+		
+		altCompareTest = a1.compareTo(a2);
+		assertEquals(1, altCompareTest);
 	}
 
 }
